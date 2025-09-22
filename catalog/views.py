1ce03982a5b26085c651d098265399fe39ceda59
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-09-22 23:21:02 +0500

    добавлены статические css u js, сделаны контроллеры

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'catalog/home.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('user_name')
+        email = request.POST.get('user_email')
+        user_text = request.POST.get('user_text')
+        print(name,user_text)
+        return render(request, 'catalog/thanks.html')
+    return render(request, 'catalog/contacts.html')
