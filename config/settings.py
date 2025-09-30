f4435cd6f63e576f816409a57b7ce1ae5887e3ed
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-09-30 22:23:32 +0500

    изменены настройки для хранения файлов

--- config/settings.py
+++ config/settings.py
@@ -134,6 +134,6 @@ STATICFILES_DIRS = [
 #Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
