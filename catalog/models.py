022d99a70a807eac8b0c718444b803abfd1aa761
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-09-30 22:20:33 +0500

    создана модель Category

--- catalog/models.py
+++ catalog/models.py
@@ -1,10 +1,23 @@
 from django.db import models
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.name}\n {self.description}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
 class Product(models.Model):
     name = models.CharField(max_length=150, verbose_name='Наименование')
     description = models.TextField( verbose_name='Описание')
-    image = models.ImageField(upload_to='images/')
-    category = models.CharField(max_length=150, verbose_name='Категория')
+    image = models.ImageField(upload_to='images/', verbose_name='Изображение')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     purchase_price = models.FloatField(verbose_name='Цена за покупку')
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
@@ -16,7 +29,3 @@ class Product(models.Model):
         verbose_name = 'продукт'
         verbose_name_plural = 'продукты'
         ordering = ['purchase_price']
-
-
-class Category(models.Model):
-    pass
