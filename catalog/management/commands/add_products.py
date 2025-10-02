4f565d2536e1318732a3d6f349aa664fb2f82187
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-10-02 21:11:41 +0500

    Создана кастомная команда для добавления тестовых продуктов и категорий.

---
+++ catalog/management/commands/add_products.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+from catalog.models import Product, Category
+
+
+class Command(BaseCommand):
+    help = 'Load test data from fixture'
+
+    def handle(self, *args, **kwargs):
+
+        Product.objects.all().delete()
+        Category.objects.all().delete()
+
+        call_command('loaddata', 'category_fixture.json')
+        self.stdout.write(self.style.SUCCESS('Successfully loaded categories from fixture'))
+
+        call_command('loaddata', 'product_fixture.json')
+        self.stdout.write(self.style.SUCCESS('Successfully loaded products from fixture'))
