4802e128d2792de1070b8ec1aeeecdb0c46c5415
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-10-03 00:21:52 +0500

    создана модель контактов

--- catalog/models.py
+++ catalog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Category(models.Model):
+
     name = models.CharField(max_length=150, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание')
 
@@ -14,6 +15,7 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+
     name = models.CharField(max_length=150, verbose_name='Наименование')
     description = models.TextField( verbose_name='Описание')
     image = models.ImageField(upload_to='images/', verbose_name='Изображение', blank=True, null=True)
@@ -29,3 +31,21 @@ class Product(models.Model):
         verbose_name = 'товар'
         verbose_name_plural = 'товары'
         ordering = ['purchase_price']
+
+
+class StoreContact(models.Model):
+
+    telephone = models.CharField(max_length=20, verbose_name='Телефон для связи', blank=False, null=False)
+    email = models.CharField(max_length=50, verbose_name='Электронная почта', blank=False, null=False)
+    main_office = models.CharField(max_length=200, verbose_name='Адрес офиса', blank=False, null=False)
+    office_hours = models.CharField(max_length=200, verbose_name='Время работы офиса', blank=False, null=False)
+    tg_contact = models.CharField(max_length=50, verbose_name='Телеграмм', blank=True, null=True)
+    whatsapp = models.CharField(max_length=50, verbose_name='Ватсапп', blank=True, null=True)
+    instagram = models.CharField(max_length=50, verbose_name='Инстаграмм', blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.main_office}\n {self.telephone}'
+
+    class Meta:
+        verbose_name = 'Контакт'
+        verbose_name_plural = 'Контакты'
