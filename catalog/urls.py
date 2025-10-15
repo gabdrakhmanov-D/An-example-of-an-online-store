205911b13aeac919201edcd4daea0ca4e40d3f93
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-10-15 16:53:19 +0500

    контроллер добавления товара, шаблон, ссылка переделаны на CreateView

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,15 +1,15 @@
 from . import views
 from django.urls import path
 
-from .views import HomeListView, ContactFormView, ProductDetailView
+from .views import HomeListView, ContactFormView, ProductDetailView, ProductCreateView
 
 app_name = 'catalog'
-
 
 urlpatterns = [
     # path('home/', views.home, name='home'),
     path("home/", HomeListView.as_view(), name="home"),
     path("contacts/", ContactFormView.as_view(), name="contacts"),
     path("product_info/<int:pk>/", ProductDetailView.as_view(), name='product_detail'),
-    path('add_product/', views.add_product, name='add_product'),
+    path("add_product/", ProductCreateView.as_view(), name="add_product"),
+    # path('add_product/', views.add_product, name='add_product'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import DetailView
 from django.views.generic.list import ListView
-from django.views.generic.edit import FormView
+from django.views.generic.edit import FormView, CreateView
 
 from catalog.forms import ContactForm
 from catalog.models import Product, StoreContact, Category
@@ -43,21 +43,36 @@ class ProductDetailView(DetailView):
     context_object_name = 'product'
 
 
-def add_product(request):
-    if request.method == 'POST':
-        name = request.POST.get('product_name')
-        description = request.POST.get('product_description')
-        image = request.FILES['product_image']
-        category_id = int(request.POST.get('product_category'))
-        category=Category.objects.get(pk=category_id)
-        purchase_price = request.POST.get('product_purchase_price')
-        Product.objects.create(name=name,
-                               description=description,
-                               image=image,
-                               category=category,
-                               purchase_price=purchase_price)
-        return render(request, 'catalog/thanks.html')
-    categories = Category.objects.all()
-    context = {'categories': categories}
-    template = "catalog/add_product.html"
-    return render(request, template_name=template, context=context)
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ['name',
+              'description',
+              'image',
+              'category',
+              'purchase_price']
+    template_name = "catalog/add_product.html"
+    success_url = reverse_lazy("catalog:home")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["categories"] = Category.objects.all()
+        return context
+
+# def add_product(request):
+#     if request.method == 'POST':
+#         name = request.POST.get('product_name')
+#         description = request.POST.get('product_description')
+#         image = request.FILES['product_image']
+#         category_id = int(request.POST.get('product_category'))
+#         category=Category.objects.get(pk=category_id)
+#         purchase_price = request.POST.get('product_purchase_price')
+#         Product.objects.create(name=name,
+#                                description=description,
+#                                image=image,
+#                                category=category,
+#                                purchase_price=purchase_price)
+#         return render(request, 'catalog/thanks.html')
+#     categories = Category.objects.all()
+#     context = {'categories': categories}
+#     template = "catalog/add_product.html"
+#     return render(request, template_name=template, context=context)
