9ed8c08a15e017fb93aec42b91f238a5471eed59
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-10-16 17:17:28 +0500

    создан контроллер и пути для удаления, обновления блога

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from blog.views import BlogListView, BlogCreateView
+from blog.views import BlogListView, BlogCreateView, BlogUpdateView, BlogDeleteView
 
 app_name = 'blog'
 
 urlpatterns =[
     path("home/", BlogListView.as_view(), name="home"),
     path("add_article/", BlogCreateView.as_view(), name="add_article"),
+    path("update_article/<int:pk>", BlogUpdateView.as_view(), name="update_article"),
+    path("blog_confirm_delete/<int:pk>", BlogDeleteView.as_view(), name="blog_confirm_delete"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView
-from django.views.generic.edit import FormView, CreateView
+from django.views.generic.edit import FormView, CreateView, UpdateView, DeleteView
 
 from blog.models import Blog
 
@@ -22,3 +22,20 @@ class BlogListView(ListView):
     # paginate_by = 3
     template_name = "blog/home.html"
     context_object_name = 'blog'
+
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ['title',
+              'content',
+              'preview',
+              'is_published',
+              ]
+    template_name = "blog/update_article.html"
+    success_url = reverse_lazy("blog:home")
+
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    template_name = 'blog_confirm_delete.html'
+    success_url = reverse_lazy('blog:home')
