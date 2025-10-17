dc7cf2d35eb09d88c8bf67205ac4160203ac3fa9
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-10-17 17:34:43 +0500

    внесены незначительные исправления, версия приложения 4.0

--- catalog/forms.py
+++ catalog/forms.py
@@ -3,5 +3,5 @@ from django import forms
 
 class ContactForm(forms.Form):
     user_name = forms.CharField(label="Имя", max_length=100)
-    user_email =forms.EmailField(label="Электронная почта")
+    user_email = forms.EmailField(label="Электронная почта")
     user_text = forms.CharField(label="Сообщение", widget=forms.Textarea)
