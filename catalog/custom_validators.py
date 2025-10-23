f0b111463fc3585f27e597bce02bcbfae6a359b6
Denis <rubik5000@yandex.com>
2025-10-23 18:00:32 +0500

    Update catalog/custom_validators.py
    Co-authored-by: painassasin <painassasin@icloud.com>

--- catalog/custom_validators.py
+++ catalog/custom_validators.py
@@ -22,7 +22,7 @@ class Validator(RegexValidator):
     def __call__(self, value):
         for word in self.FORBIDDEN_WORDS:
             self.regex = re.compile(rf'({word})', re.IGNORECASE)
-            regex_matches = self.regex.search(str(value))
+            regex_matches = self.regex.search(value)
             invalid_input = regex_matches if self.inverse_match else not regex_matches
             if invalid_input:
                 self.message = 'Слово "%(value)s", нельзя использовать при заполнении формы.'
