184fc10cf164822631d3cd9f7a9d4c41fca35911
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-10-25 20:28:37 +0500

    в меню добавлена кнопка для авторизации и регистрации. создано представление для регистрации

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from users.forms import UserRegisterForm
+
+
+class RegisterView(CreateView):
+    template_name = 'users/register.html'
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('catalog:home')
+
+    # def form_valid(self, form):
+    #     user = form.save()
+    #     self.send_welcome_email(user.email, user.username, user.password)
+    #     return super().form_valid(form)
+    #
+    # def send_welcome_email(self, user_email, login, password):
+    #     subject = 'Добро пожаловать в нашу библиотеку!'
+    #     message = f'Спасибо з а регистраицю! Ваш логин{login},\n Ваш пароль{password}'
+    #     from_email = EMAIL_HOST_USER
+    #     recipient_list = [user_email,]
+    #     send_mail(subject, message, from_email, recipient_list)
