9b2b22d6fc5b75312716f726ed41a0d77b6e089b
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-10-29 22:12:48 +0500

    исправлена ошибка в наименовании права доступа

--- catalog/views.py
+++ catalog/views.py
@@ -55,14 +55,8 @@ class ProductUpdateView(LoginRequiredMixin, UpdateView):
 class ProductDeleteView(LoginRequiredMixin, DeleteView):
     model = Product
 
-    # def get(self, request, *args, **kwargs):
-    #     product_id = kwargs['pk']
-    #     product = get_object_or_404(Product, id=product_id)
-    #     if not self.request.user.has_perm('product.can_unpublish_product'):
-    #         return HttpResponseForbidden("У вас нет доступа для удаления товара.")
-    #     return redirect("catalog/product_confirm_delete.html")
     def get_queryset(self, **kwargs):
-        if not self.request.user.has_perm('product.can_unpublish_product'):
+        if not self.request.user.has_perm('product.delete_product'):
             raise PermissionDenied("У вас нет доступа для удаления товара.")
 
         return super().get_queryset()
