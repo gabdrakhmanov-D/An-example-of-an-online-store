4c514375c55145e44bb2f356a92579ea0e9b5531
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-10-31 17:32:36 +0500

    установлен и настроен redis

--- config/settings.py
+++ config/settings.py
@@ -155,3 +155,10 @@ EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+CACHES = {
+    'default' : {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1'
+    }
+}
