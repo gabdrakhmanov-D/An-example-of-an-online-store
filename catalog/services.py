886557ee9c2d9b48c32855880f883ce8ede48a00
Denis Gabdrakhmanov <rubik5000@ya.ru>
2025-11-03 20:44:27 +0500

    добавлены докстринги в сервисные функции

--- catalog/services.py
+++ catalog/services.py
@@ -4,11 +4,15 @@ from catalog.models import Product
 
 
 def get_products_from_category(category_id):
+    """ Функция для получения списка товаров из заданной категории"""
+
     products = Product.objects.filter(category=category_id, is_publish=True)
     return products
 
 
 def get_products_from_cache():
+    """ Функция получает данные из кэша, если их нет делает запрос в БД """
+
     key = "products_list"
     products = cache.get(key)
 
